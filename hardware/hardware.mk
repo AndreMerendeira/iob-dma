d2dc5e2cba6872204dd7c1c1f4d491a384840b6d
P_Miranda <pedro_miranda2@hotmail.com>
2020-09-18 13:25:33 +0100

    hardware.mk typo

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -2,7 +2,7 @@ DMA_HW_DIR:=$(DMA_DIR)/hardware
 
 #include
 DMA_INC_DIR:=$(DMA_HW_DIR)/include
-INCLUDE+:$(incdir) $(DMA_INC_DIR)
+INCLUDE+=$(incdir) $(DMA_INC_DIR)
 
 #headers
 WHDR+=$(wildcard $(DMA_INC_DIR)/*.vh)
