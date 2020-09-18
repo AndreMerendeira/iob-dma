a46b4d22d7fd66a182f2b43bb846d7391aa7a7b7
P_Miranda <pedro_miranda2@hotmail.com>
2020-09-18 23:05:25 +0100

    Fixed dma_w wstrb signal

--- hardware/src/dma_axi_w.v
+++ hardware/src/dma_axi_w.v
@@ -84,16 +84,15 @@ module dma_axi_w # (
 
    // Data write constants
    assign m_axi_wdata = wdata;
-   assign m_axi_wstrb = wstrb;
    
    // delays
    always @ (posedge rst, posedge clk)
      if (rst) begin
-	// m_axi_wstrb <= {DMA_DATA_WIDTH/8{1'b0}};
+	m_axi_wstrb <= {DMA_DATA_WIDTH/8{1'b0}};
 	m_axi_wlast <= 0;
 	m_axi_wvalid <= 0;
      end else begin
-	// m_axi_wstrb <= wstrb;
+	m_axi_wstrb <= wstrb;
 	m_axi_wlast <= m_axi_wlast_int;
 	m_axi_wvalid <= m_axi_wvalid_int;
      end
