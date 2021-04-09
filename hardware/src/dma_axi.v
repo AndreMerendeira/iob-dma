518bd36ffc88995c03a6f49fb242c7ebce4334a5
Joao D Lopes <joao.d.lopes91@gmail.com>
2021-04-09 14:32:49 +0100

    format files

--- hardware/src/dma_axi.v
+++ hardware/src/dma_axi.v
@@ -4,129 +4,134 @@
 `include "iob_lib.vh"
 
 module dma_axi #(
-		 parameter DMA_DATA_W = 32,
-		 // AXI4 interface parameters
-		 parameter AXI_ADDR_W = `AXI_ADDR_W,
-		 parameter AXI_DATA_W = DMA_DATA_W
+                 parameter DMA_DATA_W = 32,
+                 // AXI4 interface parameters
+                 parameter AXI_ADDR_W = `AXI_ADDR_W,
+                 parameter AXI_DATA_W = DMA_DATA_W
+                 )
+   (
+    // system inputs
+    input                    clk,
+    input                    rst,
 
-		 ) (
+    // AXI4 Master i/f
+`include "cpu_axi4_m_if.v"
 
-		    // system inputs
-		    input 		     clk,
-		    input 		     rst,
+    // Native i/f - can't include from INTERCON because address_w = 6
+    input                    valid,
+    input [AXI_ADDR_W-1:0]   address,
+    input [DMA_DATA_W-1:0]   wdata,
+    input [DMA_DATA_W/8-1:0] wstrb,
+    output [DMA_DATA_W-1:0]  rdata,
+    output                   ready,
 
-		    // AXI4 Master i/f
-                    `include "cpu_axi4_m_if.v"
-
-		    // Native i/f - can't include from INTERCON because address_w = 6
-		    input 		     valid,
-		    input [AXI_ADDR_W-1:0]   address,
-		    input [DMA_DATA_W-1:0]   wdata,
-		    input [DMA_DATA_W/8-1:0] wstrb,
-		    output [DMA_DATA_W-1:0]  rdata,
-		    output 		     ready,
-
-		    // DMA signals
-		    input [`AXI_LEN_W-1:0]   dma_len,
-		    output 		     dma_ready,
-		    output 		     error
-
-		    );
+    // DMA signals
+    input [`AXI_LEN_W-1:0]   dma_len,
+    output                   dma_ready,
+    output                   error
+    );
 
    // internal wires
-   wire 				   ready_r, ready_w;
-   wire 				   dma_ready_r, dma_ready_w;
-   wire 				   error_r, error_w;
+   wire                      ready_r, ready_w;
+   wire                      dma_ready_r, dma_ready_w;
+   wire                      error_r, error_w;
 
    // assign outputs
-   assign ready = (|wstrb) ? ready_w : ready_r;
-   assign dma_ready = dma_ready_r && dma_ready_w;
+   assign ready = |wstrb? ready_w: ready_r;
+   assign dma_ready = dma_ready_r & dma_ready_w;
    assign error = error_r | error_w;
 
    // AXI_DMA READ
    dma_axi_r
      #(
-       .DMA_DATA_W(DMA_DATA_W),
-       .ADDR_W(AXI_ADDR_W)
-       )dma_r (
-	       .clk(clk),
-	       .rst(rst),
+       .ADDR_W(AXI_ADDR_W),
+       .DMA_DATA_W(DMA_DATA_W)
+       )
+   dma_r (
+          .clk(clk),
+          .rst(rst),
 
-	       // Native interface
-	       .valid    (valid && ~(|wstrb)),
-	       .addr     (address),
-	       .rdata    (rdata),
-	       .ready    (ready_r),
-	       // DMA configuration
-	       .dma_len  (dma_len),
-	       .dma_ready(dma_ready_r),
-	       .error(error_r),
-	       //address read
-	       .m_axi_arid(m_axi_arid),
-	       .m_axi_araddr(m_axi_araddr),
-	       .m_axi_arlen(m_axi_arlen),
-	       .m_axi_arsize(m_axi_arsize),
-	       .m_axi_arburst(m_axi_arburst),
-	       .m_axi_arlock(m_axi_arlock),
-	       .m_axi_arcache(m_axi_arcache),
-	       .m_axi_arprot(m_axi_arprot),
-	       .m_axi_arqos(m_axi_arqos),
-	       .m_axi_arvalid(m_axi_arvalid),
-	       .m_axi_arready(m_axi_arready),
-	       //read
-	       // .m_axi_rid(m_axi_rid),
-	       .m_axi_rdata(m_axi_rdata),
-	       .m_axi_rresp(m_axi_rresp),
-	       .m_axi_rlast(m_axi_rlast),
-	       .m_axi_rvalid(m_axi_rvalid),
-	       .m_axi_rready(m_axi_rready)
-	       );
+          // Native interface
+          .valid    (valid & ~|wstrb),
+          .addr     (address),
+          .rdata    (rdata),
+          .ready    (ready_r),
 
+          // DMA configuration
+          .dma_len  (dma_len),
+          .dma_ready(dma_ready_r),
+          .error    (error_r),
 
+          // address read
+          .m_axi_arid   (m_axi_arid),
+          .m_axi_araddr (m_axi_araddr),
+          .m_axi_arlen  (m_axi_arlen),
+          .m_axi_arsize (m_axi_arsize),
+          .m_axi_arburst(m_axi_arburst),
+          .m_axi_arlock (m_axi_arlock),
+          .m_axi_arcache(m_axi_arcache),
+          .m_axi_arprot (m_axi_arprot),
+          .m_axi_arqos  (m_axi_arqos),
+          .m_axi_arvalid(m_axi_arvalid),
+          .m_axi_arready(m_axi_arready),
 
+          // read
+          //.m_axi_rid   (m_axi_rid),
+          .m_axi_rdata (m_axi_rdata),
+          .m_axi_rresp (m_axi_rresp),
+          .m_axi_rlast (m_axi_rlast),
+          .m_axi_rvalid(m_axi_rvalid),
+          .m_axi_rready(m_axi_rready)
+          );
 
    // AXI_DMA WRITE
    dma_axi_w
      # (
-	.USE_RAM(1),
-	.DMA_DATA_W(DMA_DATA_W),
-	.ADDR_W(AXI_ADDR_W)
-	) dma_w (
-		 .clk(clk),
-		 .rst(rst),
-		 // Native interface
-		 .valid    (valid && (|wstrb)),
-		 .addr     (address),
-		 .wdata    (wdata),
-		 .wstrb    (wstrb),
-		 .ready    (ready_w),
-		 // DMA configurations
-		 .dma_len  (dma_len),
-		 .dma_ready(dma_ready_w),
-		 .error(error_w),
-		 // Address write
-		 .m_axi_awid(m_axi_awid),
-		 .m_axi_awaddr(m_axi_awaddr),
-		 .m_axi_awlen(m_axi_awlen),
-		 .m_axi_awsize(m_axi_awsize),
-		 .m_axi_awburst(m_axi_awburst),
-		 .m_axi_awlock(m_axi_awlock),
-		 .m_axi_awcache(m_axi_awcache),
-		 .m_axi_awprot(m_axi_awprot),
-		 .m_axi_awqos(m_axi_awqos),
-		 .m_axi_awvalid(m_axi_awvalid),
-		 .m_axi_awready(m_axi_awready),
-		 //write
-		 .m_axi_wdata(m_axi_wdata),
-		 .m_axi_wstrb(m_axi_wstrb),
-		 .m_axi_wlast(m_axi_wlast),
-		 .m_axi_wvalid(m_axi_wvalid),
-		 .m_axi_wready(m_axi_wready),
-		 //write response
-		 // .m_axi_bid(m_axi_bid),
-		 .m_axi_bresp(m_axi_bresp),
-		 .m_axi_bvalid(m_axi_bvalid),
-		 .m_axi_bready(m_axi_bready)
-		 );
+        .ADDR_W(AXI_ADDR_W),
+        .DMA_DATA_W(DMA_DATA_W),
+        .USE_RAM(1)
+        )
+   dma_w (
+          .clk(clk),
+          .rst(rst),
+
+          // Native interface
+          .valid    (valid & |wstrb),
+          .addr     (address),
+          .wdata    (wdata),
+          .wstrb    (wstrb),
+          .ready    (ready_w),
+
+          // DMA configurations
+          .dma_len  (dma_len),
+          .dma_ready(dma_ready_w),
+          .error    (error_w),
+
+          // Address write
+          .m_axi_awid   (m_axi_awid),
+          .m_axi_awaddr (m_axi_awaddr),
+          .m_axi_awlen  (m_axi_awlen),
+          .m_axi_awsize (m_axi_awsize),
+          .m_axi_awburst(m_axi_awburst),
+          .m_axi_awlock (m_axi_awlock),
+          .m_axi_awcache(m_axi_awcache),
+          .m_axi_awprot (m_axi_awprot),
+          .m_axi_awqos  (m_axi_awqos),
+          .m_axi_awvalid(m_axi_awvalid),
+          .m_axi_awready(m_axi_awready),
+
+          // write
+          .m_axi_wdata (m_axi_wdata),
+          .m_axi_wstrb (m_axi_wstrb),
+          .m_axi_wlast (m_axi_wlast),
+          .m_axi_wvalid(m_axi_wvalid),
+          .m_axi_wready(m_axi_wready),
+
+          // write response
+          //.m_axi_bid   (m_axi_bid),
+          .m_axi_bresp (m_axi_bresp),
+          .m_axi_bvalid(m_axi_bvalid),
+          .m_axi_bready(m_axi_bready)
+          );
 
 endmodule
--- hardware/src/dma_axi_r.v
+++ hardware/src/dma_axi_r.v
@@ -2,79 +2,79 @@
 
 `include "dma_axi.vh"
 
-// Log2 number of states
+// cLog2 number of states
 `define R_STATES_W 1
 
 // FSM States
-`define R_ADDR_HS `R_STATES_W'h0 //Read address handshake
-`define R_DATA    `R_STATES_W'h1 //Read data
+`define R_ADDR_HS `R_STATES_W'h0 // Read address handshake
+`define R_DATA    `R_STATES_W'h1 // Read data
 
 module dma_axi_r #(
-		   parameter DMA_DATA_W = 32,
-		   parameter ADDR_W = `AXI_ADDR_W
-		   ) (
+                   parameter ADDR_W = `AXI_ADDR_W,
+                   parameter DMA_DATA_W = 32
+                   )
+   (
+    // system inputs
+    input                     clk,
+    input                     rst,
 
-		      // system inputs
-		      input 			     clk,
-		      input 			     rst,
+    // Databus interface
+    output reg                ready,
+    input                     valid,
+    input [ADDR_W-1:0]        addr,
+    output [DMA_DATA_W-1:0]   rdata,
 
-    		      // Databus interface
-    		      output reg 		     ready,
-    		      input 			     valid,
-    		      input [ADDR_W-1:0] 	     addr,
-    		      output [DMA_DATA_W-1:0] 	     rdata,
+    // DMA configuration
+    input [`AXI_LEN_W-1:0]    dma_len,
+    output reg                dma_ready,
+    output                    error,
 
-		      // DMA configuration
-		      input [`AXI_LEN_W-1:0] 	     dma_len,
-		      output reg 		     dma_ready,
-		      output 			     error,
-		      
-		      // Master Interface Read Address
-		      output wire [`AXI_ID_W-1:0]    m_axi_arid,
-		      output wire [ADDR_W-1:0] 	     m_axi_araddr,
-		      output wire [`AXI_LEN_W-1:0]   m_axi_arlen,
-		      output wire [`AXI_SIZE_W-1:0]  m_axi_arsize,
-		      output wire [`AXI_BURST_W-1:0] m_axi_arburst,
-		      output wire [`AXI_LOCK_W-1:0]  m_axi_arlock,
-		      output wire [`AXI_CACHE_W-1:0] m_axi_arcache,
-		      output wire [`AXI_PROT_W-1:0]  m_axi_arprot,
-		      output wire [`AXI_QOS_W-1:0]   m_axi_arqos,
-		      output reg 		     m_axi_arvalid,
-		      input wire 		     m_axi_arready,
+    // Master Interface Read Address
+    output [`AXI_ID_W-1:0]    m_axi_arid,
+    output [ADDR_W-1:0]       m_axi_araddr,
+    output [`AXI_LEN_W-1:0]   m_axi_arlen,
+    output [`AXI_SIZE_W-1:0]  m_axi_arsize,
+    output [`AXI_BURST_W-1:0] m_axi_arburst,
+    output [`AXI_LOCK_W-1:0]  m_axi_arlock,
+    output [`AXI_CACHE_W-1:0] m_axi_arcache,
+    output [`AXI_PROT_W-1:0]  m_axi_arprot,
+    output [`AXI_QOS_W-1:0]   m_axi_arqos,
+    output reg                m_axi_arvalid,
+    input                     m_axi_arready,
 
-		      // Master Interface Read Data
-		      // input wire [`AXI_ID_W-1:0]     m_axi_rid,
-		      input wire [DMA_DATA_W-1:0]    m_axi_rdata,
-		      input wire [`AXI_RESP_W-1:0]   m_axi_rresp,
-		      input wire 		     m_axi_rlast,
-		      input wire 		     m_axi_rvalid,
-		      output reg 		     m_axi_rready
-		      );
+    // Master Interface Read Data
+    //input [`AXI_ID_W-1:0]     m_axi_rid,
+    input [DMA_DATA_W-1:0]    m_axi_rdata,
+    input [`AXI_RESP_W-1:0]   m_axi_rresp,
+    input                     m_axi_rlast,
+    input                     m_axi_rvalid,
+    output reg                m_axi_rready
+    );
 
-   //local params
-   integer 					     axi_arsize = $clog2(DMA_DATA_W/8);
-   
+   // local params
+   localparam                 axi_arsize = $clog2(DMA_DATA_W/8);
+
    // counter, state and error regs
-   reg [`AXI_LEN_W-1:0] 			      counter_int, counter_int_nxt;
-   reg [`R_STATES_W-1:0] 			      state, state_nxt;
-   reg 						      error_int, error_nxt;
+   reg [`AXI_LEN_W-1:0]       counter_int, counter_int_nxt;
+   reg [`R_STATES_W-1:0]      state, state_nxt;
+   reg                        error_int, error_nxt;
 
    // dma ready to receive run command
-   reg 				       dma_ready_nxt;
+   reg                        dma_ready_nxt;
 
    // register len and addr valid
-   reg [`AXI_LEN_W-1:0]		       len_r;
-   reg 				       m_axi_arvalid_int;
+   reg [`AXI_LEN_W-1:0]       len_r;
+   reg                        m_axi_arvalid_int;
 
-   //output error
+   // output error
    assign error = error_int;
-   
+
    // Address read constants
    assign m_axi_arid = `AXI_ID_W'b0;
    assign m_axi_araddr = addr;
-   assign m_axi_arlen = len_r; //number of trasfers per burst
-   assign m_axi_arsize = axi_arsize[`AXI_SIZE_W-1:0]; //INCR interval
-   assign m_axi_arburst = `AXI_BURST_W'b01; //INCR
+   assign m_axi_arlen = len_r; // number of trasfers per burst
+   assign m_axi_arsize = axi_arsize; // INCR interval
+   assign m_axi_arburst = `AXI_BURST_W'b01; // INCR
    assign m_axi_arlock = `AXI_LOCK_W'b0;
    assign m_axi_arcache = `AXI_CACHE_W'h2;
    assign m_axi_arprot = `AXI_PROT_W'b010;
@@ -84,30 +84,33 @@ module dma_axi_r #(
    assign rdata = m_axi_rdata;
 
    // Counter, error, state and addr valid registers
-   always @ (posedge clk, posedge rst)
-     if (rst) begin
-       state <= `R_ADDR_HS;
-       counter_int <= {`AXI_LEN_W{1'b0}};
-       error_int <= 1'b0;
-       m_axi_arvalid <= 1'b0;
-	dma_ready <= 1'b1;
-     end else begin
-       state <= state_nxt;
-       counter_int <= counter_int_nxt;
-       error_int <= error_nxt;
-       m_axi_arvalid <= m_axi_arvalid_int;
-	dma_ready <= dma_ready_nxt;
-     end
+   always @(posedge clk, posedge rst) begin
+      if (rst) begin
+         state <= `R_ADDR_HS;
+         counter_int <= `AXI_LEN_W'd0;
+         error_int <= 1'b0;
+         m_axi_arvalid <= 1'b0;
+         dma_ready <= 1'b1;
+      end else begin
+         state <= state_nxt;
+         counter_int <= counter_int_nxt;
+         error_int <= error_nxt;
+         m_axi_arvalid <= m_axi_arvalid_int;
+         dma_ready <= dma_ready_nxt;
+      end
+   end
 
    // register len
-   always @ (posedge clk, posedge rst)
-      if(rst)
-         len_r <= {`AXI_LEN_W{1'b0}};
-      else if(state == `R_ADDR_HS)
+   always @(posedge clk, posedge rst) begin
+      if (rst) begin
+         len_r <= `AXI_LEN_W'd0;
+      end else if (state == `R_ADDR_HS) begin
          len_r <= dma_len;
-   
+      end
+   end
+
    // State machine
-   always @ * begin
+   always @* begin
       state_nxt = state;
       error_nxt = error_int;
       counter_int_nxt = counter_int;
@@ -115,35 +118,42 @@ module dma_axi_r #(
       dma_ready_nxt = 1'b0;
       m_axi_arvalid_int = 1'b0;
       m_axi_rready = 1'b0;
+
       case (state)
-	    //addr handshake
-	    `R_ADDR_HS: begin
-       	       counter_int_nxt = {`AXI_LEN_W{1'b0}};
-	       dma_ready_nxt = 1'b1;
-	       if (valid) begin
-	          if (m_axi_arready == 1'b1) begin
-	             state_nxt = `R_DATA;
-		  end
-	          m_axi_arvalid_int = 1'b1;
-		  dma_ready_nxt = 1'b0;
-	       end
-	    end
-	    //data read
-	    `R_DATA: begin
-	       m_axi_rready = 1'b1;
-	       if (m_axi_rvalid == 1'b1) begin
-	          if (counter_int == len_r) begin
-	             if (m_axi_rlast == 1'b1)
-		        error_nxt = 1'b0;
-	             else
-		        error_nxt = 1'b1;
-	             state_nxt = `R_ADDR_HS;
-	          end
-	          ready = 1'b1;
-	          counter_int_nxt = counter_int + 1'b1;
-	       end
-	    end
+        // addr handshake
+        `R_ADDR_HS: begin
+           counter_int_nxt = `AXI_LEN_W'd0;
+           dma_ready_nxt = 1'b1;
+
+           if (valid) begin
+              if (m_axi_arready) begin
+                 state_nxt = `R_DATA;
+              end
+
+              m_axi_arvalid_int = 1'b1;
+              dma_ready_nxt = 1'b0;
+           end
+        end
+        // data read
+        `R_DATA: begin
+           m_axi_rready = 1'b1;
+
+           if (m_axi_rvalid) begin
+              if (counter_int == len_r) begin
+                 if (m_axi_rlast) begin
+                    error_nxt = 1'b0;
+                 end else begin
+                    error_nxt = 1'b1;
+                 end
+
+                 state_nxt = `R_ADDR_HS;
+              end
+
+              ready = 1'b1;
+              counter_int_nxt = counter_int + 1'b1;
+           end
+        end
       endcase
    end
-   
+
 endmodule
