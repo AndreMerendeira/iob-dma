ac39c09367b3a7644d7ad1811f029020d774672e
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-02-07 12:31:37 +0000

    cleanup unused submodules

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -1,22 +1,9 @@
 ifeq ($(filter DMA, $(HW_MODULES)),)
 
-MEM_DIR ?= $(DMA_DIR)/submodules/MEM
-INTERCON_DIR ?=$(DMA_DIR)/submodules/INTERCON
-LIB_DIR ?=$(DMA_DIR)/submodules/LIB
-
 #add itself to HW_MODULES list
 HW_MODULES+=DMA
 
-DMA_SRC_DIR = $(DMA_DIR)/hardware/src
-
-INCLUDE += -I $(LIB_DIR)/hardware/include
-
-#DMA HARDWARE
-
 # sources
-VSRC+=$(DMA_SRC_DIR)/iob_dma.v
-VSRC+=$(MEM_DIR)/hardware/fifo/sfifo/iob_sync_fifo.v
-VSRC+=$(MEM_DIR)/hardware/fifo/bin_counter.v
-VSRC+=$(MEM_DIR)/hardware/ram/2p_ram/iob_2p_ram.v
+VSRC+=$(DMA_DIR)/hardware/src/iob_dma.v
 
 endif
