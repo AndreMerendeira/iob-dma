06a420babcd22934626f88fb484599f339a1b949
PMiranda <pmiranda.iobundle@gmail.com>
2024-04-02 17:54:22 +0100

    feat(driver): add mmap file operation - update driver to add mmap file operation     - allocates pages with memory region for dma access     - set iob_dma base address to physical address     - user gets virtual address of same region - NOTE: initial version, still not tested

--- software/linux/drivers/iob_dma_main.c
+++ software/linux/drivers/iob_dma_main.c
@@ -9,6 +9,7 @@
 #include <linux/io.h>
 #include <linux/ioport.h>
 #include <linux/kernel.h>
+#include <linux/mm.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
@@ -20,12 +21,15 @@
 static int iob_dma_probe(struct platform_device *);
 static int iob_dma_remove(struct platform_device *);
 
+static int iob_dma_mmap(struct file *file, struct vm_area_struct *vma);
 static ssize_t iob_dma_read(struct file *, char __user *, size_t, loff_t *);
 static ssize_t iob_dma_write(struct file *, const char __user *, size_t,
                                loff_t *);
 static loff_t iob_dma_llseek(struct file *, loff_t, int);
 static int iob_dma_open(struct inode *, struct file *);
 static int iob_dma_release(struct inode *, struct file *);
+
+static void iob_dma_vm_close(struct vm_area_struct *vma);
 
 static struct iob_data iob_dma_data = {0};
 DEFINE_MUTEX(iob_dma_mutex);
@@ -39,6 +43,11 @@ static const struct file_operations iob_dma_fops = {
     .llseek = iob_dma_llseek,
     .open = iob_dma_open,
     .release = iob_dma_release,
+    .mmap = iob_dma_mmap,
+};
+
+static struct vm_operations_struct iob_dma_vm_ops = {
+    .close = iob_dma_vm_close,
 };
 
 static const struct of_device_id of_iob_dma_match[] = {
@@ -170,6 +179,21 @@ static void __exit iob_dma_exit(void) {
   platform_driver_unregister(&iob_dma_driver);
 }
 
+static void iob_dma_vm_close(struct vm_area_struct *vma) {
+    int page_mask = 0;
+    void *virtual_addr = NULL;
+    int clog2_num_pages = 0;
+    uintptr_t priv_data = (uintptr_t) vma->vm_private_data;
+
+    page_mask = (1 << PAGE_SHIFT)-1;
+    virtual_addr = (void *) (priv_data & ~page_mask);
+    clog2_num_pages = (int) (priv_data & page_mask);
+
+    pr_info("[Driver] VM: Closing virtual address: %p\n", virtual_addr);
+
+    free_pages((unsigned long) virtual_addr, clog2_num_pages);
+}
+
 //
 // File operations
 //
@@ -192,6 +216,68 @@ static int iob_dma_release(struct inode *inode, struct file *file) {
   mutex_unlock(&iob_dma_mutex);
 
   return 0;
+}
+
+static unsigned int ceil_log2(int val){
+   unsigned int clog2 = 0;
+   while((1 << clog2) < val){
+      clog2 += 1;
+   }
+   return clog2;
+}
+
+static int iob_dma_mmap(struct file *file, struct vm_area_struct *vma) {
+    unsigned long size = 0;
+    int page_size = 0;
+    unsigned long page_start = 0;
+    int num_pages = 0;
+    int clog2_num_pages = 0;
+    struct page *page = NULL;
+    void *physical_addr = NULL;
+    void *virtual_addr = NULL;
+    int res = 0;
+
+    size = (unsigned long) vma->vm_end - vma->vm_start;
+    page_size = 1 << PAGE_SHIFT;
+    pr_info("[Driver] MMAP: Page Size: %d\tsize = %lu\n", page_size, size);
+
+    // Only support page aligned sizes - size must be multiple of page_size
+    if (size % page_size != 0) {
+        pr_err("[Driver] MMAP: Size not page aligned\n");
+        return -EINVAL;
+    }
+
+    // same as ceil(size/page_size), since size = k*page_size
+    num_pages = size / page_size;
+    clog2_num_pages = ceil_log2(num_pages);
+
+    page = alloc_pages(GFP_KERNEL, clog2_num_pages);
+    if (page == NULL) {
+        pr_err("[Driver] MMAP: Failed to allocate pages\n");
+        return -ENOMEM;
+    }
+
+    page_start = page_to_pfn(page);
+    res = remap_pfn_range(vma, vma->vm_start, page_start, size, vma->vm_page_prot);
+    if (res != 0) {
+        pr_err("[Driver] MMAP: Failed to remap pages\n");
+        __free_pages(page, clog2_num_pages);
+        return -EAGAIN;
+    }
+
+    // Set BASE_ADDR to physical address
+    virtual_addr = page_address(page);
+    physical_addr = (void *) virt_to_phys(virtual_addr);
+    iob_data_write_reg(iob_dma_data.regbase, (uint32_t) physical_addr, IOB_DMA_BASE_ADDR_ADDR,
+                       IOB_DMA_BASE_ADDR_W);
+    pr_info("[Driver] BASE_ADDR iob_dma: %p\n", physical_addr);
+    
+    // Store order in the lower bits that are guaranteed to be zero
+    // because virtual_mem is page aligned
+    vma->vm_private_data = (void *) ((uintptr_t) virtual_addr | (uintptr_t) clog2_num_pages);
+    vma->vm_ops = &iob_dma_vm_ops;
+
+    return 0;
 }
 
 static ssize_t iob_dma_read(struct file *file, char __user *buf, size_t count,
